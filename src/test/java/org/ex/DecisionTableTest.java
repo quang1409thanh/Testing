934c4ecd4e946592c6de08885b39da719412a5ca
quang1409thanh <thanhaxt342@gmail.com>
2023-09-28 00:59:19 +0700

    black box testing

---
+++ src/test/java/org/ex/DecisionTableTest.java
@@ -0,0 +1,17 @@
+package org.ex;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecisionTableTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/DecisionTable.csv", numLinesToSkip = 1)
+    void test(int type, int value, String expected_output) {
+        String actual_output = Calculator.calculatePrice(type, value);
+        assertEquals(expected_output, actual_output);
+    }
+
+}
